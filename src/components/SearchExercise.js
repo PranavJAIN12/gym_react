84d01b56b687a074e9e3eeefb1c6dd48d9695bb0
PranavJAIN12 <94856533+PranavJAIN12@users.noreply.github.com>
2024-03-18 22:33:25 +0530

    search section

--- src/components/SearchExercise.js
+++ src/components/SearchExercise.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './SearchExercise.css'
+
 
 export default function SearchExercise() {
+  const[search, setSearch] = useState('Search exercise')
+
+  const handleSearch=()=>{
+    console.log("button Clicked")
+  }
+
   return (
-    <div>
-      <h1>Search</h1>
-    </div>
+    <section id="search">
+      <h1>Awesome Excercise you should know</h1>
+      <div className="search-container">
+  <input type="text" id="exercise-search" placeholder={search} onChange={(e)=>{
+
+  }}/>
+  <button id="search-btn" onClick={handleSearch}>Search</button>
+</div>
+
+    </section>
   )
 }
